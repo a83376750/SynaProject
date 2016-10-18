89c86ed09260235df5f2a7a23179a37eca3d4a92
a83376750 <394315001@qq.com>
2016-10-18 17:26:06 +0800

    在服务器读写部分添加了日志

--- src/Server.cpp
+++ src/Server.cpp
@@ -28,11 +28,18 @@ int Server::Recv(void *buffer, size_t len)
 	{
 		nBytes += zmq_recv(m_socket, buffer, len - nBytes, 0);
 	}
+	auto log = Log::get_b_mt_logger();
+
 	if (nBytes != len)
 	{
-		auto log = Log::get_b_mt_logger();
 		log->info("大小不一致,读取大小:{},实际大小:{}", nBytes, len);
 		return -1;
+	}
+	else
+	{
+		unsigned char *sBuffer = new unsigned char[len];
+		sprintf_s((char*)sBuffer, len, "%02x", buffer);
+		log->info("读取的buffer:{}", sBuffer);
 	}
 	return 0;
 }
@@ -44,9 +51,19 @@ int Server::Send(void *buffer, size_t len)
 	{
 		nBytes += zmq_send(m_socket, buffer, len - nBytes, 0);
 	}
+
+	auto log = Log::get_b_mt_logger();
+
 	if (nBytes != len)
 	{
+		log->info("大小不一致,写入大小:{},实际大小:{}", nBytes, len);
 		return -1;
+	}
+	else
+	{
+		unsigned char *sBuffer = new unsigned char[len];
+		sprintf_s((char*)sBuffer, len, "%02x", buffer);
+		log->info("写入的buffer:{}", sBuffer);
 	}
 	return 0;
 }
